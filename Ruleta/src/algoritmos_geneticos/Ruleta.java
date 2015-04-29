8541bd72a747d6c397fb39a30773aa8d3664704a
Martin Guereta <guereta.martin@gmail.com>
2015-04-28 21:35:24 -0300

    Ruleta hasta clase del 28/04

--- Ruleta/src/algoritmos_geneticos/Ruleta.java
+++ Ruleta/src/algoritmos_geneticos/Ruleta.java
@@ -1,56 +1,104 @@
-/* Trabajo Práctica Ruleta, Algoritmos Geneticos */
 package algoritmos_geneticos;
 import java.util.ArrayList;
 import java.util.Random;
-/**
 
- * @author Martin Guereta
- */
-public class Ruleta {
-    
-    /* Declaro un arreglo de String de 10x30 */
-    
-       static String[][] cromosomas = new String[10][30];
+public class Ruleta2 {
+	
+	static String[][] cromosomas = new String[10][30];
+	static int[] cromosomasDecimal = new int[10];
+	static float[] valoresEnFuncion = new float[10];
+        static float[] valoresFitness = new float[10];
+        static float sumatoriaObj=0;
+        static float sumatoriaFit=0;
+        static float maxObj=0;
+        static float maxFit=0;
+	static float pc = (float) 0.75;
+	static float pm = (float) 0.05;
+	static   float promeObj,promeFit=0;
 
-    public static void main(String[] args) {
-    
-        /* Declaro un constructor de la clase Random */
-        
-        Random rnd = new Random();
-        
-        /* Declaro un ArrayList de cromosomas del tipo String */
-        
-        ArrayList<String> cro = new ArrayList<String>();
-        
-      
-        for(int i=0;i<10;i++){
-            String c="";
-                for (int j=0;j<30;j++){
-                    /* Le doy valor a una posicion de la matriz */
-            cromosomas[i][j]= String.valueOf((int)(rnd.nextDouble()*2 +0));
-                    /* Guardo el valor del numero en esa posicion */
-            c=c+cromosomas[i][j];
-                } 
-                /* guardo el String de 30 numeros binarios en el ArrayList
-                    de cromosomas */
-     
-                cro.add(c);
-     }
-        
-         for(int i=0;i<10;i++){
-               for (int j=0;j<30;j++){
-           System.out.print(cromosomas[i][j]);
-               }      System.out.println(" "+dameDecimal(cro.get(i).toString()));
+	public static void main(String[] args)
+	{
+		Random rnd = new Random();
+		ArrayList<String> cro = new ArrayList<String>();
+		float sumatoria = 0;
+		
+		for(int i=0;i<10;i++)
+		{
+			String c = "";
+			for(int j=0;j<30;j++)
+			{
+				cromosomas[i][j] = String.valueOf((int)(rnd.nextDouble()*2+0));
+				c = c+cromosomas[i][j];
+			}
+			cro.add(c);
+			cromosomasDecimal[i] = dameDecimal(c);
+			valoresEnFuncion[i] = funcion(dameDecimal(cro.get(i).toString()));
+		}
+		
+                for(int i=0;i<10;i++)
+		{ sumatoriaObj = sumatoriaObj + valoresEnFuncion[i];
+                }
+                /*Guardo el arreglo de fittnes*/
+                 for(int i=0;i<10;i++)
+		{ 
+                    valoresFitness[i] = valoresEnFuncion[i]/sumatoriaObj;
+                    sumatoriaFit += valoresFitness[i];
+                }
+                 for(int i=0;i<10;i++){
+                     if(maxFit<valoresFitness[i]){maxFit=valoresFitness[i];};
+                   if(maxObj<valoresEnFuncion[i]){maxObj=valoresEnFuncion[i];};
+   
+                 }
+                
+                
+                promeObj=sumatoriaObj/10;
+                promeFit=sumatoriaFit/10;
+                
+                /*muestra*/
+		System.out.println("Sumatoria: "+sumatoria);
+		System.out.println("POBLACION (BINARIO)\t\tDECIMAL\tFUNCION OBJ\tFITNESS\tPORCENTAJE");
+		for(int i=0;i<10;i++)
+		{
+			for(int j=0;j<30;j++)
+			{
+				System.out.print(cromosomas[i][j]);
+			}
+			System.out.println(" "+cromosomasDecimal[i]+" "+valoresEnFuncion[i]+" "+(valoresFitness[i]/*fitness*/)+" "+(valoresFitness[i]/*fitness*/)*100);
+		
+		}
+		;
+		System.out.println("Sumatoria del Fittnes: " + Math.round(sumatoriaFit));
+                System.out.println("Sumatoria de la Objetiva: " + (sumatoriaObj));
+                System.out.println("Promedio del Fitness: " + (promeFit));
+                System.out.println("Promedio de la Objetiva: " + (promeObj));
+                System.out.println("Maximo del Fitness: " + (maxFit));
+                System.out.println("Sumatoria de la Objetiva: " + (maxObj));
 
-    	}
+
+
+
 			
+        
+        
+        
+                
 	}
 
-        /*Funcion que transforma el Binario en un decimal*/
-		private static int dameDecimal(String cromos2) {
+
+	private static int dameDecimal(String cromos2) {
 		
-		 String nroBinario = cromos2.toString();
+		String nroBinario = cromos2.toString();
 		int num = Integer.parseInt(nroBinario,2);
 		return num;
 	}
-}	
+	
+	private static float funcion(int nro)
+	{
+		int coef = (int)Math.pow(2,30) - 1;
+		float rdo = 0;
+		float div = (float)nro/(float)coef;
+		rdo = (float)Math.pow(div, 2);		
+		return rdo;
+		
+	}
+}
